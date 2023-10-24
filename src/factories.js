101bf822a016ec51f65adf5a36d46cdc9d19902b
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2023-10-24 12:22:29 -0300

    Factory Functions DONE

--- src/factories.js
+++ src/factories.js
@@ -31,10 +31,19 @@ class Gameboard {
         for (let i = 0; i < size; i += 1) {
           for (let j = 0; j < size; j += 1) {
             const coordinate = `${i},${j}`;
-            this.board[coordinate] = { hit: false, missed: false, hasShip: false, ship: [] };
+            this.board[coordinate] = { attacked: false, missed: false, hasShip: false, ship: [] };
         }
         }
         return this.board;
+      }
+
+      isValid (coordinates) {
+        let arr = coordinates.split(',');
+        let a = Number(arr[0]);
+        let b = Number(arr[1]);
+
+
+        return a >= 0 && a <= 7 && b >= 0 && b <= 7;
       }
 
       getBoard() {
@@ -46,14 +55,36 @@ class Gameboard {
       }
 
       placeShip (size, coordinates) {
+        if (size !== coordinates.length) return 'Size and coordinates length should be equal';
+
         const ship = new Ship(size);
             for (let i = 0; i < coordinates.length; i++) {
-               this.board[coordinates[i]].ship.push(coordinates);
-               this.board[coordinates[i]].hasShip = true;
+                if (this.isValid(coordinates[i])) {
+                    this.board[coordinates[i]].ship.push(coordinates);
+                    this.board[coordinates[i]].hasShip = true;
+                }
+                else if (!this.isValid(coordinates[i])) return 'No Valid';
             }
         ship.addCoordinates(coordinates);
         this.addShip(ship);
             return this.board;
+      }
+
+      receiveAttack (coordinates) {
+        let str = `${coordinates}`;
+
+        if (this.board[str].hasShip) {
+            this.board[str].attacked = true;
+            this.ships.forEach(ship => {
+                if (ship.coordinates[0].includes(str)) {
+                    ship.registerHit();
+                    if (ship.isSunk()) ship.sunk = true;
+                }
+            });
+        }
+            else {
+                this.board[str].missed = true;
+            }
       }
 
       getShips () {
@@ -68,20 +99,14 @@ class Gameboard {
 
 let board = new Gameboard();
 board.createBoard();
-board.placeShip(3, ['7,1', '7,2','7,3'])
-board.placeShip(5, ['0,1', '0,2','0,3'])
-board.ships[0].sunk = true;
-board.ships[1].sunk = true;
-
-
-// console.log(board.getShips()[0]);
-console.log(board);
-
-// Note that we have not yet created any User Interface. We should know our code is coming together by running the tests. You shouldn’t be relying on console.log or DOM methods to make sure your code is doing what you expect it to.
-// Gameboards should be able to place ships at specific coordinates by calling the ship factory function.
-// Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
-// Gameboards should keep track of missed attacks so they can display them properly.
-// Gameboards should be able to report whether or not all of their ships have been sunk.
+board.placeShip(3, ['7,1','7,2','7,3']);
+board.placeShip(3, ['0,1', '0,2','0,3'])
+board.receiveAttack('0,1');
+board.receiveAttack('0,2');
+board.receiveAttack('0,3');
+board.receiveAttack('7,1');
+board.receiveAttack('7,2');
+board.receiveAttack('7,3');
 
 
 // Player Class //
@@ -92,28 +117,32 @@ class Player {
     }
 
     isMoveLegal (move) {
-        // if gameboard includes move && gameboard[move].hit return true;
+        return !this.gameboard.board[move].attacked && !this.gameboard.board[move].missed;
     }
+
 
     makeRandomMove () {
         let num1 = Math.floor(Math.random() * 8); 
         let num2 = Math.floor(Math.random() * 8); 
-        let result = [`${num1, num2}`];
+        let result = `${num1},${num2}`;
 
-        while (!isMoveLegal(result)) {
-            num1 = Math.floor(Math.random() * 8); 
-            num2 = Math.floor(Math.random() * 8); 
-            result = [`${num1, num2}`];
+        if (this.isMoveLegal(result)) return result;
+
+        else {
+            while (!this.isMoveLegal(result)) {
+                num1 = Math.floor(Math.random() * 8); 
+                num2 = Math.floor(Math.random() * 8); 
+                result = `${num1},${num2}`;
+            }
         }
-        
         return result;
     }
 }
 
-
+const play = new Player(board);
+console.log(play.makeRandomMove());
 // Players can take turns playing the game by attacking the enemy Gameboard.
 // The game is played against the computer, so make the ‘computer’ capable of making random plays. 
-// The AI does not have to be smart, but it should know whether or not a given move is legal (i.e. it shouldn’t shoot the same coordinate twice).
 
 
 module.exports = {Ship, Gameboard, Player};
--- tests/main.test.js
+++ tests/main.test.js
@@ -3,7 +3,7 @@ const { Ship } = require('../src/factories');
 const { Gameboard } = require('../src/factories');
 const { Player } = require('../src/factories');
 
-
+// Ship //
 test('create instance of Ship', () => {
     const ship = new Ship(3);
   
@@ -33,4 +33,81 @@ test('isSunk() should return false if the ship is not sunk', () => {
   ship.registerHit();
   ship.registerHit();
   expect(ship.isSunk()).toBe(false);
+});
+
+// Gameboard // 
+describe('Gameboard', () => {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = new Gameboard();
+    gameboard.createBoard();
+  });
+
+  test('creates a game board of default size 8x8', () => {
+    const board = gameboard.getBoard();
+    expect(Object.keys(board)).toHaveLength(64);
+  });
+
+  test('adds a ship to the game board', () => {
+    const ship = ['0,0', '0,1', '0,2'];
+    gameboard.placeShip(3, ship);
+    const board = gameboard.getBoard();
+
+    for (const coordinate of ship) {
+      expect(board[`${coordinate}`].hasShip).toBe(true);
+    }
+
+    const ships = gameboard.getShips();
+    expect(ships).toHaveLength(1);
+  });
+
+  test('check validity', () => {
+    const ship = ['0,0', '0,1', '0,8'];
+    let result = gameboard.placeShip(3, ship);
+    
+    expect(result).toBe('No Valid');
+  });
+
+  test('receives an attack and registers a hit', () => {
+    const ship = ['0,0', '0,1', '0,2'];
+    gameboard.placeShip(3, ship);
+    const attackedCoordinate = '0,0';
+    gameboard.receiveAttack(attackedCoordinate);
+
+    const board = gameboard.getBoard();
+    expect(board[attackedCoordinate].attacked).toBe(true);
+
+    const ships = gameboard.getShips();
+    expect(ships[0].hit).toBe(1);
+  });
+
+  test('receives a missed attack', () => {
+    const attackedCoordinate = '0,7';
+    gameboard.receiveAttack(attackedCoordinate);
+
+    const board = gameboard.getBoard();
+    expect(board[attackedCoordinate].missed).toBe(true);
+  });
+
+  test('checks if all ships are sunk', () => {
+    const ship1 = ['0,0', '0,1', '0,2'];
+    const ship2 = ['0,3', '0,4', '0,5'];
+    gameboard.placeShip(3, ship1);
+    gameboard.placeShip(3, ship2);
+
+    // Attack and sink the first ship
+    gameboard.receiveAttack('0,0');
+    gameboard.receiveAttack('0,1');
+    gameboard.receiveAttack('0,2');
+
+    expect(gameboard.allSunk()).toBe(false);
+
+    // Attack and sink the second ship
+    gameboard.receiveAttack('0,3');
+    gameboard.receiveAttack('0,4');
+    gameboard.receiveAttack('0,5');
+
+    expect(gameboard.allSunk()).toBe(true);
+  });
 });
